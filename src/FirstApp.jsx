e632e659b4acef1d35cbb4bc313f292ae2b587fd
Antonio Mata <antoniomata99@hotmail.com>
2023-09-20 16:25:29 -0500

    new code added

---
+++ src/FirstApp.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+
+export const FirstApp = ({ title, subTitle, name }) =>{
+
+    // console.log(props);
+
+    return (
+        <>
+            <h1>{title}</h1>
+            {/* <code>{ JSON.stringify(newMessage) }</code> */}
+            <p>{subTitle}</p>
+            <p>{name}</p>
+        </>
+    );
+}
+
+FirstApp.propTypes = {
+    title: PropTypes.string.isRequired,
+    subTitle: PropTypes.string
+}
+
+FirstApp.defaultProps = {
+    name: 'Mi nombre es John Doe',
+    subTitle: 'No hay subtítulo',
+    title: 'No hay título',
+}
---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,13 @@
+// function App() {
+//     return (<h1>Hola Mundo!!</h1>);
+// }
+
+// export default App;
+
+export const HelloWorldApp = () => {
+  return (
+    <h1>Hello World App</h1>
+  );
+}
+
+// export const HelloWorldApp = () => <h1>Arrow Function</h1>;
